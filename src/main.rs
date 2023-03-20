5223817f1f3e293f8736ef88e244844c88291191
Ignacio Gomez <ignacio.gomez.arboleda@gmail.com>
2023-03-19 19:30:05 -0500

    process multiple ping

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 // Uncomment this block to pass the first stage
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+    thread,
+};
 
 fn main() {
+    let _ping = "ping";
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
@@ -11,14 +16,28 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
-                let raw_msg = b"+PONG\r\n";
-                _stream.write(raw_msg).unwrap();
+                thread::spawn(move || {
+                    handle_client(stream);
+                });
             }
+
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buf = [0; 1024];
+
+    loop {
+        let bytes_read = stream.read(&mut buf).unwrap();
+        if bytes_read == 0 {
+            return;
+        }
+        stream.write("+PONG\r\n".as_bytes()).unwrap();
     }
 }
